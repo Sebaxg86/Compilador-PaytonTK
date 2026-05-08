166a50963df99d46642cc0d7c63f5c2be1e5ff75
Sebastian Chairez <sebax86@icloud.com>
2026-05-07 23:37:09 -0600

    feat: Analisis Semántico Implementado

---
+++ src/compilador/Atributos.java
@@ -0,0 +1,12 @@
+package compilador;
+
+import java.util.ArrayList;
+
+public class Atributos {
+    String tipo = "";
+    String h = "";
+    String tmp = "";
+    String ret = "";
+    ArrayList<String> listaIds = new ArrayList<String>();
+    ArrayList<String> listaTipos = new ArrayList<String>();
+}
